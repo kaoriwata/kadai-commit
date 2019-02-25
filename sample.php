2be10bd6b2046908f273db02e6fca2fa728b2878
kaori.watanabe <aobozu0620@gmail.com>
2019-02-25 12:55:10 +0000

    add omikuji function

--- sample.php
+++ sample.php
@@ -1,10 +1,21 @@
+<?php
+    $result = '無し';
+    if (array_key_exists('act', $_POST)) {
+        $result = omikuji();
+    }
+    
+    function omikuji() {
+        $fortune = ["大吉", "吉", "中吉", "小吉", "末吉", "凶"];
+        return $fortune[random_int(0, count($fortune) - 1)];
+    }
+?>
 <!DOCTYPE html>
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
         <title>おみくじ</title>
     </head>
-     <body>
+    <body>
         <h1>おみくじ</h1>
         <p>おみくじの結果：<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
         <form action="sample.php" method="POST">
